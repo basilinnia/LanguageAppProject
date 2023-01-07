4ad3946c1d0650f425e296a8fd54ef92cdcffa68
devbasilinnia <dev.ecemk01@gmail.com>
2023-01-07 16:31:34 +0300

    additional update

---
+++ app/src/main/java/com/appexamples/firebaseauthjetpackcompose/presentation/main_screen/CircleBar.kt
@@ -0,0 +1,93 @@
+package com.appexamples.firebaseauthjetpackcompose.presentation.main_screen
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun CircularProgressbar(
+    number: Float,
+    numberStyle: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    size: Dp = 83.dp,
+    indicatorThickness: Dp = 14.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0,
+    foregroundIndicatorColor: Color = Color(0xFF021C5E),
+    backgroundIndicatorColor: Color = Color.DarkGray.copy(alpha = 0.5f)
+) {
+
+    // It remembers the number value
+    var progress by remember {
+        mutableStateOf(0f)
+    }
+
+    // Number Animation
+    val animateNumber = animateFloatAsState(
+        targetValue = progress,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animationDelay
+        )
+    )
+
+    // This is to start the animation when the activity is opened
+    LaunchedEffect(Unit) {
+        progress = number
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(size = size)
+    ) {
+        Canvas(
+            modifier = Modifier
+                .size(size = size)
+        ) {
+
+            // Background circle
+            drawCircle(
+                color = backgroundIndicatorColor,
+                radius = size.toPx() / 2,
+                style = Stroke(width = indicatorThickness.toPx(), cap = StrokeCap.Round)
+            )
+
+            val sweepAngle = (animateNumber.value / 100) * 360
+
+            // Foreground circle
+            drawArc(
+                color = foregroundIndicatorColor,
+                startAngle = -90f,
+                sweepAngle = sweepAngle,
+                useCenter = false,
+                style = Stroke(indicatorThickness.toPx(), cap = StrokeCap.Round)
+            )
+        }
+
+        // Text that shows number inside the circle
+        Text(
+            text = "%" + (animateNumber.value).toInt().toString(),
+            style = numberStyle
+        )
+    }
+}
+
